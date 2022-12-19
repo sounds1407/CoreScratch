cda6e84d5dbbdf634085937571b1dd1332048f37
sounds1407 <soundaryalakshmanan99@gmail.com>
2022-12-20 01:17:14 +0530

    Ilogger implementation

--- Middlewares/DIMiddleware.cs
+++ Middlewares/DIMiddleware.cs
@@ -6,19 +6,22 @@ namespace CoreScratch.Middlewares
     {
         private RequestDelegate _next;
         private IResponseFormatter _format;
-
+        
 
         public DIMiddleware(RequestDelegate next, IResponseFormatter format)
         {
             _next = next;
             _format = format;
+            
         }
 
-        public async Task Invoke(HttpContext context)
+        public async Task Invoke(HttpContext context,ILogger<DIMiddleware> logme)
         {
 
             if (context.Request.Path == "/dimiddleware")
                 await _format.Format(context, "Dependency Injected Successfully");
+
+            logme.LogDebug("DI middleware is called!");
 
             await _next(context);
         }
